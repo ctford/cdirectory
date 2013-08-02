bb76a2aa593e276e2ce5efd1c9202b5a6e2e6578
Chris Ford <christophertford@gmail.com>
2013-08-02 18:23:28 +0300

    Added blank Rails project.

---
+++ cdirectory/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cdirectory::Application.config.secret_token = '87c0069798f1311271d464fc2caf2af1aaa3a7351cddff9931b7915c9138af09a249af1eb244a4835afd2918cd44f1ea605fd8c2c2c677d95ba57504e7f094e7'
